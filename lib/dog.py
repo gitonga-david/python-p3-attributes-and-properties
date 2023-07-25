1250368c5b7dee5b73ab00766629301f5d81254a
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2023-07-25 13:03:27 +0300

    complete

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,22 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Fido", breed="Pug"):
+        self.name = name
+        self.breed = breed
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter    
+    def name(self, value):
+        self._name = value if type(value) == str and len(value) > 0 and len(value) < 26 else print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed
+    
+    @breed.setter
+    def breed(self, value):
+        self._breed = value if value in APPROVED_BREEDS else print("Breed must be in list of approved breeds.")
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,22 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self,name="Test", job="Purchasing"):
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return  self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value.title() if type(value) == str and len(value) > 1 and len(value) < 25 else print("Name must be string between 1 and 25 characters.")
+    
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        self._job = value if value in APPROVED_JOBS else print("Job must be in list of approved jobs.")
